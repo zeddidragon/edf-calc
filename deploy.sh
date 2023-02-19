948483dded7a00f0411f758caad96dec402ee054
Zeddy <tas@scoutgg.com>
2023-02-19 14:31:51 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cp src/weapons-41.js ../zeddidragon/src/
+cp src/weapons.js ../zeddidragon/src/
 cp src/weapons-41.json ../zeddidragon/src/
+cp src/weapons-5.json ../zeddidragon/src/
 cp index.html ../zeddidragon/
 cp style.css ../zeddidragon/

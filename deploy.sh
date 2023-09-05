538d693529befd02aa7e19aba893d50673951230
Tony <tas@scoutgg.com>
2023-09-05 12:38:45 +0700

    add new files to deploy yscript

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cp src/weapons.js ../zeddidragon/src/
+cp src/weapons-3.json ../zeddidragon/src/
+cp src/weapons-3p.json ../zeddidragon/src/
 cp src/weapons-41.json ../zeddidragon/src/
 cp src/weapons-5.json ../zeddidragon/src/
 cp index.html ../zeddidragon/
